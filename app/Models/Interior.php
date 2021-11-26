7447e9abd483efc056b4479dc3befffe524f7240
Alexander Krasnoperov <a.krasnoperov@ottoruss.ru>
2021-11-26 21:22:49 +0300

    update css

--- app/Models/Interior.php
+++ app/Models/Interior.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Models\Traits\AutoAliasTrait;
+use App\Models\Traits\Images;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -26,6 +27,7 @@ class Interior extends Model
 {
     use HasFactory;
     use AutoAliasTrait;
+    use Images;
 
     public const STORE_PATH = 'public/interiors';
 
