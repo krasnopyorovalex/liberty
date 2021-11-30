c62d5a0dfe3dda6fec47699ac3ec8a51531ee206
Alexander Krasnoperov <a.krasnoperov@ottoruss.ru>
2021-11-30 11:57:33 +0300

    update css

--- app/Domain/SliderImage/Commands/CreateSliderImageCommand.php
+++ app/Domain/SliderImage/Commands/CreateSliderImageCommand.php
@@ -29,11 +29,14 @@ class CreateSliderImageCommand
      */
     public function handle(): bool
     {
+        $maxPosition = SliderImage::select('pos')->max('pos');
+
         $sliderImage = new SliderImage();
         $sliderImage->basename = $this->uploadImage->getImageHashName();
         $sliderImage->ext = $this->uploadImage->getExt();
         $sliderImage->slider_id = $this->uploadImage->getEntityId();
         $sliderImage->is_mobile = $this->uploadImage->getIsMobile();
+        $sliderImage->pos = $maxPosition + 1;
 
         return $sliderImage->save();
     }
