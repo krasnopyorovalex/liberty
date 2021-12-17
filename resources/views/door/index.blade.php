cf5917854adb913690d712ef9e26e6289d1b9962
Alexander Krasnoperov <a.krasnoperov@ottoruss.ru>
2021-12-17 10:38:48 +0300

    update css

--- resources/views/door/index.blade.php
+++ resources/views/door/index.blade.php
@@ -153,13 +153,13 @@
                                         <div class="row">
                                             <div class="col-12">
                                                 @if(is_mobile())
-                                                    <div class="flex finishing-options">
+                                                    <div class="flex finishing-options lazy-box">
                                                         @foreach($door->textures->chunk(6) as $chunk)
                                                             <div class="row">
                                                                 @foreach($chunk as $texture)
                                                                     <div class="col-2">
                                                                         <a href="{{ asset($texture->path) }}" data-lightbox="textures">
-                                                                            <img src="{{ asset($texture->path) }}" alt="{{ $texture->label }}" />
+                                                                            <img data-src="{{ asset($texture->path) }}" alt="{{ $texture->label }}" />
                                                                         </a>
                                                                     </div>
                                                                 @endforeach
