53634e84701040d51bdd074be6562e6ef0f1a78f
Alexander Krasnoperov <a.krasnoperov@ottoruss.ru>
2021-12-18 14:25:10 +0300

    update css

--- resources/views/layouts/first-screens/page-index-mobile.blade.php
+++ resources/views/layouts/first-screens/page-index-mobile.blade.php
@@ -2,7 +2,10 @@
     <div>
         @foreach($images->take(1) as $image)
         <div class="main-slider-item">
-            <img src="{{ $image->getPath() }}" alt="{{ $image->alt }}" title="{{ $image->title }}">
+            <picture>
+                <source media="(max-width: 670px)" srcset="{{ $page->image_mob }}">
+                <img src="{{ $image->getPath() }}" alt="{{ $image->alt }}" title="{{ $image->title }}">
+            </picture>
             <div class="main-slider-item-desc">
                 <div class="visible-sm main-slider-item-desc-mob uppercase">собственное производство</div>
             </div>
